56ace66ab978b8c06a0cc058aee13f72a9b570d8
Brian <brianma360@gmail.com>
2026-09-12 03:29:13 -0400

    add windows msvc version check for stdbool.h

--- include/levikno/lvn_config.h
+++ include/levikno/lvn_config.h
@@ -54,7 +54,7 @@
 #endif
 
 // boolean type
-#if (defined(__STDC__) && __STDC_VERSION__ >= 199901L)
+#if (defined(__STDC__) && __STDC_VERSION__ >= 199901L) || (defined(_MSC_VER) && _MSC_VER >= 1800)
     #include <stdbool.h>
 #elif !defined(__cplusplus) && !defined(bool)
     typedef enum bool { false = 0, true = !false } bool;
